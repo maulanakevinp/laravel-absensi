97c2236b08ebec9e1249a6f4f10eb0f613ec237c
ojanck <izzan.fuad@gmail.com>
2020-02-22 21:19:38 +0700

    database seed

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -20,13 +20,29 @@ class UsersTableSeeder extends Seeder
             'foto'      => 'default.jpg',
             'password'  => Hash::make('123456789')
         ]);
-        
+
         DB::table('users')->insert([
             'role_id'   => '2',
             'nama'      => 'Izzan',
             'nrp'       => '987654321',
             'foto'      => 'default.jpg',
             'password'  => Hash::make('987654321')
+        ]);
+
+        DB::table('users')->insert([
+            'role_id'   => '2',
+            'nama'      => 'Barep',
+            'nrp'       => '112233445',
+            'foto'      => 'default.jpg',
+            'password'  => Hash::make('barep123')
+        ]);
+
+        DB::table('users')->insert([
+            'role_id'   => '2',
+            'nama'      => 'Bawul Ganteng',
+            'nrp'       => '123987456',
+            'foto'      => 'default.jpg',
+            'password'  => Hash::make('bawul123')
         ]);
     }
 }
